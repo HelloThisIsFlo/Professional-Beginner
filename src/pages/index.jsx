77096017b27a6de889859b86a71cca74862fe393
Florian Kempenich <Flori@nKempenich.com>
2019-08-08 20:14:04 +0100

    Experiment w/ dynamic content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/header";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 
+const CurrentSecondsCounter = () => {
+  const getCurrentSeconds = () => new Date().getSeconds();
+
+  const [secondWhenPageLoaded, _] = useState(getCurrentSeconds());
+  const [currentSeconds, setCurrentSeconds] = useState(secondWhenPageLoaded);
+
+  useEffect(() => {
+    const updateEverySecond = setInterval(() => {
+      setCurrentSeconds(getCurrentSeconds());
+    }, 1000);
+    return () => {
+      clearInterval(updateEverySecond);
+    };
+  }, []); // Pass [] to run the effect only once, not every render
+
+  return (
+    <div>
+      <h3>Seconds</h3>
+      <table>
+        <tr>
+          <th>When page loaded:</th>
+          <td>{secondWhenPageLoaded}</td>
+        </tr>
+        <tr>
+          <th>Currently: </th>
+          <td>{currentSeconds}</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
 export default ({ data }) => {
   const blogPosts = data.allMarkdownRemark.edges.map(({ node }) => node);
   const blogPosts3Times = [...blogPosts, ...blogPosts, ...blogPosts];
+
   return (
     <Layout>
-      {/* <Header headerText={data.site.siteMetadata.title} /> */}
       <p>
         <Link to="/about-css-modules/">About CSS Modules</Link>
       </p>
+      <h2>Dynamic content</h2>
+      <CurrentSecondsCounter />
       <p>
         <Link to="/sandbox">Sandbox</Link>
       </p>
