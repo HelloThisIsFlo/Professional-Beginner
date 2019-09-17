d6af8b3538ef143b1f8fd5395fd6c2cb3bd9f1a0
Florian Kempenich <Flori@nKempenich.com>
2019-09-17 15:09:29 +0100

    Fix import

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 import postStyles from "../templates/post.module.scss";
 import { addExtraFormatting } from "../templates/post";
+import { graphql  } from "gatsby";
 
 export default ({ data }) => {
   const about = data.markdownRemark;
