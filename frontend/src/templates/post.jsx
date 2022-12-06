1f98d697e95d3f4dece79c21a1ed5dc84845ecd2
Florian Kempenich <Flori@nKempenich.com>
2022-12-06 13:04:30 +0000

    Add 'time to read' and 'date' at the top of each post

--- frontend/src/templates/post.jsx
+++ frontend/src/templates/post.jsx
@@ -3,28 +3,34 @@ import React from "react";
 import {graphql} from "gatsby";
 import Layout from "../components/layout";
 import {Disqus} from "gatsby-plugin-disqus";
-import SEO from "../components/seo";
+import Seo from "../components/seo";
 import addExtraFormatting from "../utils/addExtraFormatting";
+import moment from "moment/moment";
 
 const Post = ({data, location}) => {
   const post = data.markdownRemark;
   const postUrl = data.site.siteMetadata.siteUrl + location.pathname;
   const heroImage = data.file.childImageSharp.fixed;
   const disqusConfig = {
+
     url: postUrl,
     identifier: post.fields.slug.slice(1),
     title: post.frontmatter.title
   };
 
+  const formattedDate = moment(post.frontmatter.date).format("MMMM Do, YYYY");
   return (
       <Layout>
-        <SEO
+        <Seo
             title={post.frontmatter.title}
             image={heroImage}
             description={post.excerpt}
         />
         <div className={styles.post}>
           <h1 id="post-title">{post.frontmatter.title}</h1>
+          <p className={styles.readTimeAndDate}>
+            {post.timeToRead} min read - {formattedDate}
+          </p>
           <div
               dangerouslySetInnerHTML={{__html: addExtraFormatting(post.html)}}
           ></div>
@@ -43,6 +49,7 @@ export const query = graphql`
       fields {
         slug
       }
+      timeToRead
       frontmatter {
         title
         date
